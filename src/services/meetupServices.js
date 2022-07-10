b801a4220a437298caa0040b6d95a44c238def32
Ruslan <c8man24@gmail.com>
2022-07-11 00:34:39 +0300

    feature: add servives

---
+++ src/services/meetupServices.js
@@ -0,0 +1,44 @@
+import { NotFoundError } from '../errors/NotFoundError.js'
+import { NotUniqueError } from '../errors/NotUniqueError.js'
+import { models } from '../models/index.js'
+
+export const meetupServices = {
+  add(data) {
+    return models.meetup.create(data)
+  },
+
+  readAll() {
+    return  models.meetup.findAll()
+  },
+
+  async readById(id) {
+    const meetup = await models.meetup.findByPk(id)
+    if (!meetup) {
+      throw new NotFoundError(`Doesn't exist such id: ${id}`)
+    }
+    return meetup
+  },
+
+  async update(id, changes) {
+    const allMeetups = await this.readAll()
+    allMeetups.forEach(item => {
+      if(item.id === changes.id) {
+        throw new NotUniqueError(`new id must be unique`)
+      }
+    });
+    const status = await models.meetup.update(changes, { where: { id: id } })
+    if (!status) {
+      throw new NotFoundError(`Doesn't exist such id: ${id}`)
+    }
+    return this.readById(id)
+  },
+
+  async delete(id) {
+    const meetup = this.readById(id)
+    const status = await models.meetup.destroy({ where: { id: id } })
+    if (!status) {
+      throw new NotFoundError(`Doesn't exist such id: ${id}`)
+    }
+    return meetup
+  },
+}
