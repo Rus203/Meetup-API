60c1e329fb543ca7117a03f1144d7f51bc4ff234
Ruslan <c8man24@gmail.com>
2022-07-11 22:01:25 +0300

    feature: add express

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+
+import { routing } from './routers/index.js'
+import { sequelize } from './models/index.js'
+
+const HOST = process.env.HOST
+const PORT = process.env.PORT
+
+
+const server = express()
+
+sequelize.sync({ alter: true })
+
+server.use(express.json())
+
+server.use(routing)
+
+sequelize.sync({ alter: true }).then(
+  () => console.log('the synchronization was completed successfully'),
+  err => console.log(err)
+)
+
+server.listen(PORT, HOST, () => {
+  console.log(`the server is running on the ${PORT} port`)
+})
