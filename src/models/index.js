b41cc23a64c86502e114cc704f31fd67242d0c3f
Ruslan <c8man24@gmail.com>
2022-07-20 02:26:22 +0300

    feature: add the models for users and

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,9 @@
-import { Sequelize, DataTypes } from 'sequelize';
-import { meetupModel } from './meetupModel.js';
+import { Sequelize, DataTypes } from 'sequelize'
+import { meetupModel } from './meetupModel.js'
+import { userModel } from './userModel.js'
+import { refreshTokenModel } from './refreshTokenModel.js'
+import { roleModel } from './roleModel.js'
+import { userRoleModel } from './userRoleModel.js'
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,14 +12,23 @@ export const sequelize = new Sequelize(
   {
     dialect: 'postgres',
     host: process.env.DB_HOST,
-    logging: false
+    logging: false,
   }
-);
+)
 
 export const models = {}
 models.meetupModel = meetupModel(sequelize, DataTypes)
-// ...
+models.userModel = userModel(sequelize, DataTypes)
+models.refreshTokenModel = refreshTokenModel(sequelize, DataTypes)
+models.userRoleModel = userRoleModel(sequelize, DataTypes)
+models.roleModel = roleModel(sequelize, DataTypes)
 
+models.userModel.belongsToMany(models.roleModel, {
+  through: models.userRoleModel,
+})
+models.roleModel.belongsToMany(models.userModel, {
+  through: models.userRoleModel,
+})
 
-
-
+models.userModel.hasMany(models.refreshTokenModel)
+models.refreshTokenModel.belongsTo(models.userModel)
