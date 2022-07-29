4f59c28662fdd48e079a36796fa49422d3405a25
Ruslan <c8man24@gmail.com>
2022-07-29 17:18:05 +0300

    feature: add checking uuid middleware

--- src/routers/meetupRouters.js
+++ src/routers/meetupRouters.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import meetupControllers from '../controllers/meetupControllers.js';
 
 import asyncWrapperMiddleware from '../middleware/asyncWrapperMiddleware.js';
+import isUUIDMiddleware from '../middleware/isUUIDMiddleware.js';
 
 const meetupRouters = Router();
 
 meetupRouters.get('/', meetupControllers.readAll);
 meetupRouters.post('/', asyncWrapperMiddleware(meetupControllers.add));
+
+meetupRouters.use('/:id/', isUUIDMiddleware);
 
 meetupRouters.get('/:id', asyncWrapperMiddleware(meetupControllers.readOne));
 meetupRouters.put('/:id', asyncWrapperMiddleware(meetupControllers.update));
