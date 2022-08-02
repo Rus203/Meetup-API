852e9e0238eef9083833fea38e988dd65460e036
Ruslan <c8man24@gmail.com>
2022-08-03 00:02:23 +0300

    refactor: refactor server.js and include option dependences

--- src/server.js
+++ src/server.js
@@ -1,25 +1,32 @@
 import 'dotenv/config';
 import express from 'express';
+import passport from 'passport';
 
 import routings from './routers/index.js';
 import sequelize from '../sequelize/index.js';
+import strategy from '../passport/index.js';
 import errorHandleMiddleware from './middleware/errorHandleMiddleware.js';
+import { initRoles, deleteFiredTokens } from '../sequelize/databaseUtils.js';
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(routings);
-
 sequelize.sync({ alter: true }).then(
   () => {
     console.log('All models have been synchronized');
+    initRoles();
+    deleteFiredTokens();
   },
   (error) => console.log(error)
 );
+
+passport.use(strategy);
+app.use(passport.initialize());
+
+app.use(express.json());
+app.use(routings);
 
 app.use(errorHandleMiddleware);
 
